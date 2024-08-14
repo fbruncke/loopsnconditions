99f79276f3705ccad56d045ab387261e45ec679f
Frank <fha@easv.dk>
2024-08-14 11:29:30 +0200

    Add project files.

---
+++ loopsnconditions/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace loopsnconditions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.comboBox1.Location = new System.Drawing.Point(250, 46);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(108, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(407, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 86);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Test condition";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(407, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.comboBox2.Location = new System.Drawing.Point(250, 86);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(108, 24);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(407, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.comboBox3.Location = new System.Drawing.Point(250, 129);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(108, 24);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Location = new System.Drawing.Point(60, 26);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(508, 190);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "short circuit";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 246);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 38);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Break";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(95, 326);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 36);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "continue";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(95, 468);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 36);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "short hand add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(468, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(468, 346);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(310, 255);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 22);
+            this.textBox1.TabIndex = 15;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(95, 382);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 57);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Continue while";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(95, 570);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(153, 57);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "By reference";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(553, 570);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(153, 57);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "Scope";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(310, 570);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(153, 57);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "keyword out";
+            this.button8.UseMnemonic = false;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.keyOut);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(775, 188);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 20;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(752, 570);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(121, 57);
+            this.button10.TabIndex = 21;
+            this.button10.Text = "Recursion";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 696);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Lopos and conditions";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ loopsnconditions/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace loopsnconditions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //button1.Location = new Point(1, 2);
+
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            //Thread.Sleep(1000);
+            //sw.Stop();
+            //int time = sw.Elapsed.Seconds;
+
+            //MessageBox.Show("time: " + time);
+
+
+        }
+
+        #region short circuit
+
+        /// <summary>
+        /// Test condition
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (getcbox(comboBox1, label1).Equals("1")
+                && getcbox(comboBox2, label2).Equals("1")
+                && getcbox(comboBox3, label3).Equals("1"))
+            {
+                MessageBox.Show("they are all one");
+            }
+            else
+            {
+                MessageBox.Show("short circuit evaluation rules");
+            }
+        }
+
+        private string getcbox(ComboBox cbox, Label lb)
+        {
+            MessageBox.Show("test");
+            string returnVal = cbox.Text;
+            lb.Text = returnVal;
+            return returnVal;
+        }
+
+        #endregion
+
+
+        #region mixed
+
+        /// <summary>
+        /// increment
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {                      
+
+            int a = 5, b = 0;
+            b = ++a;
+            MessageBox.Show("b is: " + b);
+
+            a = 5;            
+            b = a++;
+            MessageBox.Show("b is then: " + b);
+
+        }
+
+        /// <summary>
+        /// break
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i;
+            for ( i = 0; i < 100; i++)
+            {
+                if (textBox1.Text.Equals(i.ToString()))
+                {
+                    break;
+                }
+            }
+            MessageBox.Show("I am done counting, I reached: "+i);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i < 95)
+                {
+                    continue;
+                }
+                MessageBox.Show("counted to: "+i);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {            
+            Random random = new Random();
+            while (true)
+            {
+                // Get a random number.
+                int value = random.Next();
+                // If number is divisible by two, continue.
+                if ((value % 2) == 0)
+                {
+                    continue;
+                }
+                // If number is divisible by three, continue.
+                if ((value % 3) == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine("Not divisible by 2 or 3: {0}", value);
+                // Pause.
+                Thread.Sleep(100);
+            }
+
+        }
+
+        #endregion 
+
+        //---------------------------------------------------
+        #region ref
+
+        class ByValue
+        {
+            public int MyInt { get; set; }
+        }
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //int no = 20;
+            //int no2 = 10;
+            //increaseNo( ref no, ref no2);
+            //MessageBox.Show("my number:"+no+" - " + no2);
+
+            ByValue bv = new ByValue();
+            bv.MyInt = 42;
+
+            ChangeByValue(ref bv);
+            MessageBox.Show("Caller" + bv.MyInt);
+        }
+
+        private void ChangeByValue( ref ByValue byValue)
+        {
+            MessageBox.Show("" + byValue.MyInt);
+            byValue = new ByValue();
+            byValue.MyInt = 99;
+        }
+
+
+        private void increaseNo( ref int myNo, ref int myNo2) {
+            myNo = 40;
+            myNo2 = 50;            
+        }
+
+        #endregion
+
+        //---------------------------------------------------
+
+        #region scope
+
+        private int varI = 30 ;
+        private void button7_Click(object sender, EventArgs e)
+        {            
+
+            int i;            
+            for ( i = 0; i < 10; i++)
+            {
+                
+                Console.WriteLine("my number"+i);
+            }
+            MessageBox.Show("my number:"+i);            
+           
+            int varI = 50;
+            Console.WriteLine("varI:" + varI);            
+        }
+
+        private void Another() {
+            int varI = 20;
+            Console.WriteLine("varI1:" + varI);
+        }
+
+        #endregion
+
+        #region out
+        private void keyOut(object sender, EventArgs e)
+        {
+            int undefinedInt;
+            int someInt;
+
+            SetIntValue(out undefinedInt,out someInt);
+
+            MessageBox.Show("value of undefinedInt:" + undefinedInt);
+
+        }
+
+        private void SetIntValue(out int definedInt, out int definedInt2)
+        {
+            definedInt = 10;
+            definedInt2 = 20;
+        }
+
+        private void SetPrivateIntValue( int definedInt)
+        {
+            definedInt = 10;
+        }
+        #endregion
+
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int no = 5;
+
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(no++ * i);
+            }
+            Console.WriteLine(no);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            RecursionCount(5,0);
+        }
+
+        private void RecursionCount(int n, int factor)
+        {
+
+            Console.WriteLine($"{n} * {factor} = {n * factor}");
+
+            if (++factor <= 10)
+                RecursionCount(n, factor);
+        }
+    }
+}
